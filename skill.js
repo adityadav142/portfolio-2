713fde56b08d08286357e1ffc5c8db41d008a7d3
adityadav142 <adityadav0502@gmail.com>
2025-01-05 21:26:51 +0530

    added skill.js

---
+++ skill.js
@@ -0,0 +1,8 @@
+// script.js
+const typed = new Typed(".skills-content p", {
+    strings: ["HTML5", "CSS3", "JavaScript", "React", "Node.js", "Git & GitHub"],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true,
+  });
+  
